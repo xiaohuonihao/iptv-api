869bbeac3b4e42023448f5303e3f75b587247c17
xiaohuo <115022532+xiaohuonihao@users.noreply.github.com>
2026-06-05 03:08:05 +0800

    Update retry.py

--- utils/retry.py
+++ utils/retry.py
@@ -7,7 +7,7 @@ if config.open_driver:
         from selenium.webdriver.support.ui import WebDriverWait
         from selenium.webdriver.support import expected_conditions as EC
         from selenium.common.exceptions import TimeoutException
-    except:
+    except ImportError:
         pass
 
 max_retries = 2
